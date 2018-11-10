dcd87becdc3c0a2cb0b299277dee261edeac2c48
Mich <miceli.frc@gmail.com>
2018-11-10 20:45:29 +0100

    Other files added

---
+++ cmake-build-debug/CMakeFiles/RPGgame.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RPGgame.dir/main.cpp.o"
+  "RPGgame.pdb"
+  "RPGgame"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RPGgame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
